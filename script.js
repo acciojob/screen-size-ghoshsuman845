ac3b99541c98b814acb2eef408355de11fe6c4d8
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-04-30 13:46:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelector('#sizeInfo');
+const width = sizeInfo.querySelector('h1:first-child');
+const height = sizeInfo.querySelector('h1:last-child');
+
+function setScreenSize() {
+    width.innerText = `Width: ${window.innerWidth}px`;
+    height.innerText = `Height: ${window.innerHeight}px`;
+}
+
+window.addEventListener('resize', setScreenSize);
+
+setScreenSize();
